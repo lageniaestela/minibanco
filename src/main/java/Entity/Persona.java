13e76f5035c446d18ef779206dfc6503f77456c0
lageniaestela <estelam.escobar@gmail.com>
2023-05-11 11:10:30 -0300

    Initial commit

---
+++ src/main/java/Entity/Persona.java
@@ -0,0 +1,13 @@
+package Entity;
+
+import jakarta.Persistence.*;
+
+
+public class Persona {
+    private long id;
+    private String Nombre;
+    private String Apellido;
+    private int edad;
+    private long idSaldo;
+}
+}
---
+++ src/main/java/Entity/Saldo.java
@@ -0,0 +1,9 @@
+package Entity;
+
+public class Saldo {
+
+    private long idsal;
+    private long saldo;
+    private int cuenta;
+
+}
